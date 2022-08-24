62b5ddda2bc2eefef5700f683b5d8f23fe1de20b
NicholasZervos <nicholaszervos25@gmail.com>
2022-08-24 18:43:36 -0400

    updated generate-site.js

--- utils/generate-site.js
+++ utils/generate-site.js
@@ -11,7 +11,7 @@ const writeFile = fileContent => {
 
       resolve({
         ok: true,
-        message: 'File created!'
+        message: 'Profile created!'
       });
     });
   });
@@ -20,7 +20,7 @@ const writeFile = fileContent => {
 // copying file
 const copyFile = () => {
   return new Promise((resolve, reject) => {
-    fs.copyFile('./src/style.css', './dist/style.css', err => {
+    fs.copyFile('./source/style.css', './source/style.css', err => {
       if (err) {
         reject(err);
         return;
